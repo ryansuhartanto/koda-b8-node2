091208f917bb79b770f91ca98bde3c3ae004b8e7
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-05-27 12:07:08 +0700

    test: fix import source

--- src/utils-manual.test.js
+++ src/utils-manual.test.js
@@ -1,7 +1,7 @@
 import { describe, it } from "node:test";
 import assert from "node:assert";
 
-import { processDate } from "./utils.js";
+import { processDate } from "./utils-manual.js";
 
 describe("processDate manual", () => {
 	it("throws on empty parameter", () => {
